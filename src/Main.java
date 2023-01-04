13706b4406c6d2cbbb37369ac9fe289cf9fc4925
DPfeyfer <Skydenfer737@gmail.com>
2023-01-04 11:23:16 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,32 @@ public class Main {
 
         //task 1
         System.out.println("Task1");
-
+        var clientOs = 8;
+        switch (clientOs) {
+            case 0:
+                System.out.println("Download app for Android");
+                break;
+            case 1:
+                System.out.println("Download app for IOS");
+                break;
+            default:
+                System.out.println("Choose between 0 or 1");
+        }
 
 
         //task 2
         System.out.println("Task2");
+        var userOs = 1;
+        var yearDevice = 2016;
+        if(userOs == 0 && yearDevice <= 2015){
+            System.out.println("Download light version of the app for Android");}
+        else if (userOs == 1 && yearDevice <= 2015){
+            System.out.println("Download light version of the app for IOS");}
+        else if (userOs == 0 && yearDevice > 2015){
+            System.out.println("Download app for Android");}
+        else if (userOs == 1 && yearDevice > 2015){
+            System.out.println("Download app for IOS");}
+        else
 
 
         //task 3
